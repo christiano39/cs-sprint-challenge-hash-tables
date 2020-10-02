0785f82edd43f43fb156fa3eec9af5ae137df071
christiano39 <carneson39@gmail.com>
2020-10-02 09:07:29 -0600

    implement ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,12 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    d = {}
+    for i in range(length):
+        d[weights[i]] = i
+
+    for i in range(length):
+        if (limit - weights[i]) in d:
+            return (d[limit - weights[i]], i)
 
     return None
